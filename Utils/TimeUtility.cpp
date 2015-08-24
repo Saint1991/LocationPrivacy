237a80c284ec1ee4203fa732a263c7a5c99bd295
Saint1991 <se-mizuno@hotmail.co.jp>
2015-08-24 17:22:39 +0900

    TimeUtilityを修正

--- Utils/TimeUtility.cpp
+++ Utils/TimeUtility.cpp
@@ -19,13 +19,14 @@ namespace Time
 	const std::string TimeUtility::current_timestamp()
 	{
 		time_t now;
+		tm tm_now;
 		time(&now);
-		tm* tm_now = localtime(&now);
+		errno_t result = localtime_s(&tm_now, &now);
 		
 		std::stringstream timestamp;
-		int year = tm_now->tm_year + 1900;
-		int month = tm_now->tm_mon + 1;
-		timestamp << year << month << tm_now->tm_mday << tm_now->tm_min << tm_now->tm_sec;
+		int year = tm_now.tm_year + 1900;
+		int month = tm_now.tm_mon + 1;
+		timestamp << year << month << tm_now.tm_mday << tm_now.tm_min << tm_now.tm_sec;
 		return timestamp.str();
 	}
 }
