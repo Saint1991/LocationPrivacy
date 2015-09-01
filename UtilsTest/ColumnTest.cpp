a33471d498b0f431fb8b841536050e13d53d3105
shu_lab <usg_swim_omst1022-others@yahoo.co.jp>
2015-09-01 17:52:13 +0900

    ColumnTest.cpp TableStructureTest.cpp TimerTest.cpp TimeUtilityTest.cpp 修正完了

--- UtilsTest/ColumnTest.cpp
+++ UtilsTest/ColumnTest.cpp
@@ -54,6 +54,29 @@ namespace UtilsTest
 
 			Logger::WriteMessage(column2.to_string().c_str());
 		}
-		
+
+		TEST_METHOD(Column_operators)
+		{
+			std::string column_name1 = "nishio";
+			std::string type1 = "INT";
+			std::list<std::string> options1 = { "PRIMARY KEY", "AUTO INCREMENT" };
+			Db::Column column1(column_name1, type1, options1);
+
+			std::string column_name2 = "nishio";
+			std::string type2 = "INT";
+			std::list<std::string> options2 = { "AUTO INCREMENT", "PRIMARY KEY" };
+			Db::Column column2(column_name2, type2, options2);
+
+			std::string column_name3 = "nishio";
+			std::string type = "INT";
+			std::list<std::string> options3 = { "UNIQUE", "PRIMARY KEY" };
+			Db::Column column3(column_name3, type, options3);
+
+			Assert::IsTrue(column1 == column2);
+			Assert::IsFalse(column2 == column3);
+			Assert::IsFalse(column1 != column2);
+			Assert::IsTrue(column2 != column3);
+		}
+				
 	};
 }
