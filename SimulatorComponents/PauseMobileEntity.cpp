c5a3c01a7950ede0e70e1d1fd5b3208c911491f0
shu_lab <usg_swim_omst1022-others@yahoo.co.jp>
2015-10-14 18:22:43 +0900

    PauseMobileEntityの配置場所をSimulatorComponentsに変更

--- SimulatorComponents/PauseMobileEntity.cpp
+++ SimulatorComponents/PauseMobileEntity.cpp
@@ -4,6 +4,10 @@
 
 namespace Entity
 {
+
+	///<summary>
+	/// コンストラクタ
+	///</summary>
 	template <typename POSITION_TYPE>
 	PauseMobileEntity<POSITION_TYPE>::PauseMobileEntity(entity_id id, std::shared_ptr<Time::TimeSlotManager const> timeslot)
 		: MobileEntity<POSITION_TYPE>(id, timeslot)
